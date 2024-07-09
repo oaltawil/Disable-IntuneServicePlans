70abc67eac0c63ea508a4a7ee3aeee6b85de43a1
Osama Altawil <oaltawil@microsoft.com>
2024-07-09 10:49:14 -0400

    Corrected INTUNE_EDU service plan id

--- Get-IntuneLicenses.ps1
+++ Get-IntuneLicenses.ps1
@@ -3,7 +3,7 @@
 # The Organization.Read.All permission scope is required to read the licenses available in the tenant
 Connect-MgGraph -Scopes "Group.Read.All", "User.Read.All", "Directory.Read.All", "Organization.Read.All" -NoWelcome
 
-$IntuneServicePlanIds = @("c1ec4a95-1f05-45b3-a911-aa3b0e1992c1", "a23b959c-7ce8-4e57-9140-b90eb88a9e97")
+$IntuneServicePlanIds = @("c1ec4a95-1f05-45b3-a911-aa3b0e1992c1", "da24caf9-af8e-485c-b7c8-e73336da2693")
 
 foreach ($License in Get-MgSubscribedSku) {
 
